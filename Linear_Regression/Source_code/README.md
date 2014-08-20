c66549bef85f5006bf15fb974be2c98848ea26e2
bha123 <nexusbharath@gmail.com>
2014-08-20 18:04:27 +0530

    Folder deleted
    Author : Bharath P

--- Linear_Regression/Source_code/README.md
+++
@@ -1,4 +0,0 @@
-MachineLearningInAction
-=======================
-
-This Repository is created inorder to share  the work of  "Machine Learning in Action " Group in machine learning course of coursera
--- Linear_Regression/Source_code/ReadCSV.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def readFile(nameoffile):
-    with open(nameoffile, 'rb') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            print ', '.join(row)
